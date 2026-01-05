549d6a8943583191b76414e1e0407e3fb1377e39
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2026-01-05 13:24:48 +0100

    Styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
 export function Footer() {
-  return <footer>Her er en footer der er flot!</footer>;
+  return (
+    <footer>Her er en footer der er flot og lavet med kærlighed og dejlig iskold faxe kondi i foråret 2026!</footer>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export function Navbar() {
   return (
-    <nav>
+    <nav className="Navbar">
       <ul>
         <li>blablablablabla</li>
         <li>blablablablabla</li>
